bb0575faabf152d0200154273118ebb48d0876a9
Daria Bazueva <daria@darias-mbp.myfiosgateway.com>
2020-08-07 23:50:40 -0400

    Updates

---
+++ precache-manifest.ac92623f951f7d1dae99818ac427b1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce249fb726e1fb0f2a9df26844ca7b5",
+    "url": "/bazuevad/sorting-algos.git/index.html"
+  },
+  {
+    "revision": "a253829883937be59a23",
+    "url": "/bazuevad/sorting-algos.git/static/css/main.40b3a901.chunk.css"
+  },
+  {
+    "revision": "1a99f5b295fbb71e919d",
+    "url": "/bazuevad/sorting-algos.git/static/js/2.902be2ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bazuevad/sorting-algos.git/static/js/2.902be2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a253829883937be59a23",
+    "url": "/bazuevad/sorting-algos.git/static/js/main.0886d044.chunk.js"
+  },
+  {
+    "revision": "48644baa1c776df93ee5",
+    "url": "/bazuevad/sorting-algos.git/static/js/runtime-main.10bcde09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bazuevad/sorting-algos.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
